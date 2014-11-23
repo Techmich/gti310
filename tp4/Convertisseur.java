7aec64c89330e02d0d36b75f87dffff74e64e71b
Techmich <sciencesmichel@gmail.com>
2014-11-23 14:12:52 -0500

    debut
    fichier

---
+++ tp4/Convertisseur.java
@@ -0,0 +1,79 @@
+package gti310.tp4;
+
+public class Convertisseur {
+
+	float[][][] imageConvertie;
+	int[][] infoImage;
+	int[][] imageDCT;
+	
+	public Convertisseur(int[][][] image){
+		
+		imageConvertie = convertirBMP(image);
+		int[] infoImage = infosImage(image);
+		int[][] imageDCT;
+		
+	}
+	
+	public int[] infosImage(int[][][] image){
+		
+		int hauteur,largeur;
+		hauteur = image[0].length;
+		largeur = image[0][0].length;
+		
+		int[] infoImage = new int[2];
+		infoImage[0] = hauteur;
+		infoImage[1] = largeur;
+		
+		return infoImage;
+	}
+	
+	public float[][][] convertirBMP(int[][][] imageAConvertir){
+		int hauteurImage,largeurImage,valeur;
+		
+		hauteurImage = (infosImage(imageAConvertir))[0];
+		largeurImage = (infosImage(imageAConvertir))[1];
+		valeur = 0;
+		
+		float[][][] imageConvertie = new float[3][hauteurImage][largeurImage];
+		
+		for(int h = 0; h < hauteurImage; h++){
+
+			for(int l = 0; l < largeurImage; l++){
+				
+				//System.out.println(h + " Hauteur");
+				//System.out.println(l + " Largeur");
+
+				
+					if (valeur==0) {
+						imageConvertie[valeur][h][l] = ((float)((0.299*imageAConvertir[0][h][l])+(0.587*imageAConvertir[1][h][l])+(0.114*imageAConvertir[2][h][l])));
+						System.out.println(imageConvertie[valeur][h][l]);
+						//System.out.println(valeur + " Valeur");
+						valeur++;
+					}
+				
+					if (valeur==1) {
+						imageConvertie[valeur][h][l] = ((float)((-0.1687*imageAConvertir[0][h][l])+(-0.3313*imageAConvertir[1][h][l])+(0.5*imageAConvertir[2][h][l])+128));
+						//System.out.println(imageConvertie[valeur][h][l]);
+						//System.out.println(valeur + " Valeur");
+						valeur++;
+					}
+
+					if (valeur==2) {
+						imageConvertie[valeur][h][l] = ((float)((0.5*imageAConvertir[0][h][l])+(-0.4187*imageAConvertir[1][h][l])+(-0.0813*imageAConvertir[2][h][l])+128));
+						//System.out.println(imageConvertie[valeur][h][l]);
+						//System.out.println(valeur + " Valeur");
+						valeur = 0;
+					}
+					
+					
+			
+			}
+		}
+		return imageConvertie;
+
+	}
+	}
+
+//	public static int[][] ()
+//}
+
